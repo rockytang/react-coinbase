a76e92a3ad7a75dd7000720a95e8ce4a84919825
Rocky Tang <rocky.tang168@gmail.com>
2018-09-18 17:52:20 -0700

    Send ETH

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import store from './store';
 import Dashboard from './containers/Dashboard';
 
-import logo from './logo.svg';
-import './App.css';
-
-
 class App extends Component {
   render() {
     return (
